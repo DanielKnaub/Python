4f7e1c0bf81241fdbc6742383e49cadae86ea455
DanielKnaub <103882875+DanielKnaub@users.noreply.github.com>
2022-05-04 19:12:06 +0800

    Update birthdays.py

--- Python/Тема 8/birthdays.py
+++ Python/Тема 8/birthdays.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-first_date = dt.datetime(2006, 11, 18)
+first_date = dt.datetime(2006, 5, 4)
 
 second_date = dt.datetime(2006, 12, 27)
 
@@ -12,5 +12,12 @@ date_list = [first_date, second_date, fourth_date, third_date]
 
 now = dt.datetime.now()
 
+counter = 0
 for i in range(4):
-    if 
+    if date_list[i].strftime('%d.%m.') == now.strftime('%d.%m.'):
+        print('У одного из ваших друзей сегодня День рождения!')
+        counter += 1
+        
+
+if counter == 0:
+    print('Никто из ваших друзей не отмечает сегодня День рождения')
